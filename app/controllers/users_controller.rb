16ef9abb8b2a85c680d4d033689157130ee25faf
kanako.minami <supernatural.787@ezweb.ne.jp>
2020-12-30 08:06:08 +0000

    post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
+  
   def index
-    @users = User.order(id: :desc).page(params[:page]).per(25)
+     @users = User.order(id: :desc).page(params[:page]).per(25)
   end
-
+  
   def show
     @user = User.find(params[:id])
     @tasks = @user.tasks.order(id: :desc).page(params[:page])
@@ -11,10 +11,10 @@ class UsersController < ApplicationController
   end
 
   def new
-    @user = User.new
   end
-
- def create
+ 
+  
+  def create
     @user = User.new(user_params)
 
     if @user.save
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
     end
- end
+  end
 
   private
 
